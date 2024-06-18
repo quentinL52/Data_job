a2b12beda3e773e584cf2137345e594bfb9a619f
quentinL52 <158512396+quentinL52@users.noreply.github.com>
2024-06-18 12:24:48 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
 def clef_api_deepgram():
-    return "e7603b0a344c0fe0bb8dacc8964d158a069f1111"
+    return ""
     
 def clef_api_groq():
-    return "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"
+    return ""
 
 def clef_api_nomic():
-    return "nk-FMyYMSsmCxRXzhtzU4Pvpzgp5dFkC4tD1vlE0Vvd8uw"
+    return ""
 
 def clef_api_pinecone():
-    return "b863d58f-a067-464f-8183-36221176cc71"
+    return ""
