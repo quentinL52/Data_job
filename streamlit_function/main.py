8813d34af07d01ae35461a21548180d9042a676c
terry cornelusse <cornelusse.terry@gmail.com>
2024-06-27 15:34:00 +0000

    Update main.py

--- streamlit_function/main.py
+++ streamlit_function/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from file_python.manage_log import login_page, reset_password
 from file_python.yaml_config import yaml_config
 from apscheduler.schedulers.background import BackgroundScheduler
-from scraping.main_scrap import update
+from streamlit_function.scraping.main_scrap import update
 
 
 
