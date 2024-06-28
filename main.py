4b3575210e24a98afb21472c2cf86685468b4e96
terry cornelusse <cornelusse.terry@gmail.com>
2024-06-28 07:57:41 +0000

    clear file

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-import os
-import subprocess
-import sys
-
-# Ajouter le répertoire contenant vos modules personnalisés au chemin de recherche de Python
-sys.path.append('/mount/src/data_job/')
-
-# Installer Chromium
-def install_chromium():
-    subprocess.run(['apt-get', 'update'], check=True)
-    subprocess.run(['apt-get', 'install', '-y', 'chromium-browser'], check=True)
-    # Créer un lien symbolique pour Google Chrome
-    if not os.path.exists('/usr/bin/google-chrome'):
-        os.symlink('/usr/bin/chromium-browser', '/usr/bin/google-chrome')
-
-# Appeler la fonction d'installation de Chromium
-install_chromium()
-
-
 import streamlit as st
 from streamlit_function.file_authentificator.manage_log import login_page, reset_password
 from streamlit_function.file_authentificator.yaml_config import yaml_config
